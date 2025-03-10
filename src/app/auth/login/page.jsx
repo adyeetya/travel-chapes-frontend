0fb384d3f52ef7605c58692983273105f33fd5cb
adyeetya <2612adityasingh2000@gmail.com>
2025-03-10 10:24:42 +0530

    backend ip

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -66,7 +66,7 @@ export default function LoginPage() {
                 {loading ? "Processing..." : "Login"}
               </button>
               <p className="mt-4 text-sm text-center">
-                Don't have an account?{" "}
+                Don&apos;t have an account?{" "}
                 <Link
                   href="/auth/signup"
                   className="text-yellow-600 font-medium"

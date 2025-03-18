b9656af911b875b55893281714754a6a6c7f2d63
adyeetya <2612adityasingh2000@gmail.com>
2025-03-18 11:13:44 +0530

    new hero

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,67 +1,68 @@
-'use client'
-import React, { useState, useEffect, useRef } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { StatsSection } from "../weekendCarousel/weekendCarousel";
 import {
   BrandsCarousel,
   BrandsCarouselContent,
   BrandsCarouselItem,
-} from '../../components/ui/bannerCarousel'
+} from "../../components/ui/bannerCarousel";
 // import { destinations } from '@/data/destinations/destinations'
-import { IoIosSearch } from 'react-icons/io'
+import { IoIosSearch } from "react-icons/io";
 const webBanners = [
-  { id: 1, image: '/images/homepage/banner1.webp' },
-  { id: 2, image: '/images/homepage/banner2.webp' },
-  { id: 3, image: '/images/homepage/banner3.webp' },
-  { id: 4, image: '/images/homepage/banner4.webp' },
-]
+  { id: 1, image: "/images/homepage/banner1.webp" },
+  { id: 2, image: "/images/homepage/banner2.webp" },
+  { id: 3, image: "/images/homepage/banner3.webp" },
+  { id: 4, image: "/images/homepage/banner4.webp" },
+];
 const phoneBanners = [
-  { id: 1, image: '/images/homepage/phonebanner1.webp' },
-  { id: 2, image: '/images/homepage/phonebanner2.webp' },
-  { id: 3, image: '/images/homepage/phonebanner3.webp' },
-  { id: 4, image: '/images/homepage/phonebanner4.webp' },
-]
+  { id: 1, image: "/images/homepage/phonebanner1.webp" },
+  { id: 2, image: "/images/homepage/phonebanner2.webp" },
+  { id: 3, image: "/images/homepage/phonebanner3.webp" },
+  { id: 4, image: "/images/homepage/phonebanner4.webp" },
+];
 
 const debounce = (func, delay) => {
-  let timer
+  let timer;
   return (...args) => {
-    clearTimeout(timer)
-    timer = setTimeout(() => func(...args), delay)
-  }
-}
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay);
+  };
+};
 const Banner = () => {
-  const [isInView, setIsInView] = useState(true)
-  const [visibleBanners, setVisibleBanners] = useState(webBanners.slice(0, 2))
-  const bannerRef = useRef(null)
+  const [isInView, setIsInView] = useState(true);
+  const [visibleBanners, setVisibleBanners] = useState(webBanners.slice(0, 2));
+  const bannerRef = useRef(null);
 
   useEffect(() => {
     // Set a timer to update visible banners after 2 seconds
     const timer = setTimeout(() => {
-      setVisibleBanners(webBanners) // Show all banners
-    }, 2000)
+      setVisibleBanners(webBanners); // Show all banners
+    }, 2000);
 
     // Cleanup the timer when the component unmounts or the effect re-runs
-    return () => clearTimeout(timer)
-  }, [webBanners])
+    return () => clearTimeout(timer);
+  }, [webBanners]);
 
   useEffect(() => {
     const handleScroll = debounce(() => {
       if (bannerRef.current) {
-        const { top, bottom } = bannerRef.current.getBoundingClientRect()
-        setIsInView(top < window.innerHeight && bottom >= 0)
+        const { top, bottom } = bannerRef.current.getBoundingClientRect();
+        setIsInView(top < window.innerHeight && bottom >= 0);
       }
-    }, 100)
+    }, 100);
 
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div ref={bannerRef} className="relative w-full">
       <BrandsCarousel
         autoplay={isInView}
         opts={{
-          align: 'start',
+          align: "start",
           loop: true,
         }}
         className="w-full"
@@ -84,42 +85,44 @@ const Banner = () => {
         </BrandsCarouselContent>
       </BrandsCarousel>
     </div>
-  )
-}
+  );
+};
 
 const PhoneBanner = () => {
-  const [isInView, setIsInView] = useState(true)
-  const [visibleBanners, setVisibleBanners] = useState(phoneBanners.slice(0, 2))
-  const bannerRef = useRef(null)
+  const [isInView, setIsInView] = useState(true);
+  const [visibleBanners, setVisibleBanners] = useState(
+    phoneBanners.slice(0, 2)
+  );
+  const bannerRef = useRef(null);
 
   useEffect(() => {
     // Set a timer to update visible banners after 2 seconds
     const timer = setTimeout(() => {
-      setVisibleBanners(webBanners) // Show all banners
-    }, 2000)
+      setVisibleBanners(webBanners); // Show all banners
+    }, 2000);
 
     // Cleanup the timer when the component unmounts or the effect re-runs
-    return () => clearTimeout(timer)
-  }, [webBanners])
+    return () => clearTimeout(timer);
+  }, [webBanners]);
 
   useEffect(() => {
     const handleScroll = debounce(() => {
       if (bannerRef.current) {
-        const { top, bottom } = bannerRef.current.getBoundingClientRect()
-        setIsInView(top < window.innerHeight && bottom >= 0)
+        const { top, bottom } = bannerRef.current.getBoundingClientRect();
+        setIsInView(top < window.innerHeight && bottom >= 0);
       }
-    }, 100)
+    }, 100);
 
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div ref={bannerRef} className="relative w-full h-[calc(100vh-3rem)]">
       <BrandsCarousel
         autoplay={isInView}
         opts={{
-          align: 'start',
+          align: "start",
           loop: true,
         }}
         className="w-full h-screen"
@@ -142,36 +145,36 @@ const PhoneBanner = () => {
         </BrandsCarouselContent>
       </BrandsCarousel>
     </div>
-  )
-}
+  );
+};
 
 const Hero = () => {
   // console.log(product)
-  const [searchQuery, setSearchQuery] = useState('')
-  const [searchResults, setSearchResults] = useState([])
-  const [destinations, setDestinations] = useState(null)
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [destinations, setDestinations] = useState(null);
 
   useEffect(() => {
     // Asynchronously load destinations data
     const loadDestinations = async () => {
-      const { destinations } = await import('@/data/destinations/destinations')
-      setDestinations(destinations)
-    }
+      const { destinations } = await import("@/data/destinations/destinations");
+      setDestinations(destinations);
+    };
 
-    loadDestinations()
-  }, [])
+    loadDestinations();
+  }, []);
 
   // Function to handle the search
   const handleSearch = (event) => {
-    event.preventDefault() // Prevent page reload on form submit
-    if (searchQuery.trim() === '') return // Prevent search if query is empty
+    event.preventDefault(); // Prevent page reload on form submit
+    if (searchQuery.trim() === "") return; // Prevent search if query is empty
     const filteredDestinations = destinations.filter(
       (destination) =>
         destination.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
         destination.metaTitle.toLowerCase().includes(searchQuery.toLowerCase())
-    )
-    setSearchResults(filteredDestinations)
-  }
+    );
+    setSearchResults(filteredDestinations);
+  };
 
   return (
     <div className="relative z-0 w-full h-[100vh] md:min-h-screen rounded-br-[2rem] -mt-16 overflow-hidden customCurve">
@@ -179,14 +182,14 @@ const Hero = () => {
       <div className=" absolute inset-0 overflow-hidden rounded-b-3xl">
         {/* <Banner /> */}
         <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="absolute inset-0 w-full h-full object-cover"
-      >
-        <source src="/images/videos/Main_banner.webm" type="video/mp4" />
-      </video>
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute inset-0 w-full h-full object-cover"
+        >
+          <source src="/images/videos/Main_banner.webm" type="video/mp4" />
+        </video>
       </div>
 
       {/* Mobile Image */}
@@ -196,50 +199,50 @@ const Hero = () => {
 
       <div className="absolute mt-16 inset-0 z-10 flex flex-col justify-between items-start px-4">
         {/* texts */}
-        <div className="flex w-full md:mt-12 mb-20 py-4 h-full justify-start items-end md:items-center overflow-hidden max-w-screen-xl mx-auto">
-          <div className="text-gray-100 text-left z-10">
+        <div className=" flex flex-col justify-end w-full  mb-20 py-4 h-full items-center md:items-center overflow-hidden max-w-screen-xl mx-auto">
+          <div className=" text-gray-100 text-center z-10">
             <h1
-              className="text-4xl md:text-6xl mb-1 2xl:text-6xl text-left text-gray-100 tracking-wide 2xl:tracking-widest"
+              className="text-3xl md:text-5xl mb-1 2xl:text-6xl text-gray-100 tracking-wide 2xl:tracking-widest"
               style={{
-                textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)',
-                WebkitTextStroke: '0.1px #b8b8b8',
+                textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+                WebkitTextStroke: "0.1px #b8b8b8",
               }}
             >
               Chase Adventures
             </h1>
             <h1
-              className="text-4xl md:text-6xl text-left text-gray-100 tracking-wide 2xl:tracking-widest"
+              className="text-3xl md:text-5xl text-gray-100 tracking-wide 2xl:tracking-widest"
               style={{
-                textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)',
-                WebkitTextStroke: '0.1px #b8b8b8',
+                textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+                WebkitTextStroke: "0.1px #b8b8b8",
               }}
             >
               Create Memories
             </h1>
             <p
-              className="text-lg md:text-2xl mb-12"
+              className="text-base md:text-xl mb-12"
               style={{
-                textShadow: '1px 1px 3px rgba(0, 0, 0, 0.5)',
+                textShadow: "1px 1px 3px rgba(0, 0, 0, 0.5)",
               }}
             >
               TravelChapes Where dreams meet destinations
             </p>
 
-            <div className="flex gap-4 items-center">
+            <div className="flex justify-center gap-4 items-center">
               <form onSubmit={handleSearch} className="flex gap-4 items-center">
                 <input
                   type="text"
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                   placeholder="Search for destinations"
-                  className="bg-white focus:outline-none backdrop-filter backdrop-blur-lg border border-white/30 z-50 w-full text-gray-900 py-1 px-6 md:px-12 rounded-full text-[17px] md:text-lg"
+                  className="bg-white/20 backdrop-blur-xl border border-white/40 shadow-lg w-full md:w-96 text-black py-3 px-8 md:px-14 rounded-full text-lg md:text-xl focus:outline-none focus:ring-2 focus:ring-white/50"
                 />
                 <button
                   type="submit"
                   disabled={!searchQuery.trim()}
-                  className="bg-white backdrop-filter backdrop-blur-lg border border-white/30 z-50 w-fit text-gray-700 p-1 rounded-full text-sm hover:bg-black hover:text-gray-100"
+                  className="bg-white/30 backdrop-blur-xl border border-white/40 shadow-md w-fit text-gray-700 p-2 rounded-full text-sm hover:bg-black hover:text-gray-100 transition-all"
                 >
-                  <IoIosSearch className="w-6 md:w-8 md:h-8 h-6" />
+                  <IoIosSearch className="w-7 md:w-9 md:h-9 h-7" />
                 </button>
               </form>
             </div>
@@ -262,6 +265,7 @@ const Hero = () => {
               </div>
             )}
           </div>
+          <StatsSection />
         </div>
 
         {/* explore products */}
@@ -324,7 +328,7 @@ const Hero = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;

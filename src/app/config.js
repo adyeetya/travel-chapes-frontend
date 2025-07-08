e099ab0fbddafd215cc21a5dfcab271c16682c33
adyeetya <2612adityasingh2000@gmail.com>
2025-07-08 22:25:53 +0530

    ip fix

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,3 @@
-export const ServerUrl = 'http://localhost:5009/api/v1'
+export const ServerUrl = 'https://backend.travelchapes.com/api/v1'
 
 // 'http://13.49.44.64:5009/api/v1'; 'https://backend.travelchapes.com/api/v1' http://localhost:5009/api/v1
